d9a12bce42d35b133aaedc062b961ecc303f0e30
Sinkerine <git@15cm.net>
2017-07-18 16:32:31 +0800

    [+] Switch between light and dark theme

---
+++ switch-theme.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+powerline_config_file="$HOME/.config/powerline/config.json"
+cur_theme=$(jq -r '.current_theme' "$powerline_config_file")
+jq_tmp=$(mktemp)
+if [[ $cur_theme == 'light' ]]; then
+  jq '.current_theme = "dark" 
+  | .ext.shell.colorscheme = "nord" 
+  | .ext.tmux.colorscheme = "nord"' \
+     $powerline_config_file > $jq_tmp && mv $jq_tmp $powerline_config_file
+  sed -i 's/set background=\w*/set background=dark/' ~/.vimrc.theme
+else
+  jq '.current_theme = "light" 
+  | .ext.shell.colorscheme = "solarized-light" 
+  | .ext.tmux.colorscheme = "solarized-light"' \
+     $powerline_config_file  > $jq_tmp && mv $jq_tmp $powerline_config_file
+  sed -i 's/set background=\w*/set background=light/' ~/.vimrc.theme
+fi
+powerline-daemon --replace
+tmux source ~/.tmux.conf
