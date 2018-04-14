343d536e6e5f546681f30d19e1b5f52c7d82d6eb
15cm <sinkerine@gmail.com>
2018-04-14 16:33:32 -0400

    [!] add linebreak to gitignore local [+] ignore idea folder

--- local/bin/fetch_gitignore.py
+++ local/bin/fetch_gitignore.py
@@ -21,10 +21,10 @@ local_ignore_file = os.path.expanduser('~/.gitignore_global.local')
 ignore_file = os.path.expanduser('~/.gitignore_global')
 
 ignore_src_links = map(lambda x: github_link_template.format(x), ignore_src_names)
-remote_ignore_texts = map(lambda link: requests.get(link).text, ignore_src_links)
+remote_ignore_texts = list(map(lambda link: requests.get(link).text, ignore_src_links))
 with open(local_ignore_file) as f:
     local_ignore_lines = f.readlines()
 
 with open(ignore_file, 'w') as f:
-    f.writelines(local_ignore_lines + list(map(lambda txt: txt + '\n', remote_ignore_texts)))
+    f.writelines(map(lambda x: x + '\n', local_ignore_lines + remote_ignore_texts))
 
