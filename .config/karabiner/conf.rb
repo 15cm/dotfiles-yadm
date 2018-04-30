dc51319ae940afbdc50a793359b8583d3459c306
15cm <sinkerine@gmail.com>
2018-04-29 23:29:57 -0400

    refactor: modulize structure
    - put hash gen functions and const vars into modules
    - separate test

---
+++ .config/karabiner/conf.rb
@@ -0,0 +1,66 @@
+require './karabiner'
+
+module Conf
+  module_function
+  def basic_rules
+    [
+      Rule.gen(
+        "(internal) fn-caps_lock: caps_lock",
+        [
+          {
+            conditions: [
+              $cond_is_internal_keyboard
+            ],
+            from: {
+              key_code: "caps_lock",
+              modifiers: ModFrom.mandatory_fn
+            },
+            to: [
+              {key_code: "caps_lock"}
+            ],
+          }
+        ],
+      )
+    ]
+  end
+
+  # Layer1
+  def layer1_mods
+    [
+    "command",
+    "shift",
+    "option",
+    ]
+  end
+  def layer1_keys
+    KeyRegion.whole
+  end
+
+  # final conf
+  def layers
+    [
+      Layer1.new(layer1_mods, layer1_keys),
+    ]
+  end
+
+  def gen_rules()
+    layers.inject(basic_rules) { |acc, layer| acc + layer.gen_rules }
+  end
+  def gen()
+    {
+      global: {
+        check_for_updates_on_startup: true,
+        show_in_menu_bar: false,
+        show_profile_name_in_menu_bar: false,
+      },
+      profiles: [{
+        complex_modifications: {
+          parameters: {
+            "basic.to_if_alone_timeout_milliseconds": 500,
+          },
+          rules: gen_rules()
+        }
+      }]
+    }
+  end
+end
--- .config/karabiner/karabiner.rb
+++ .config/karabiner/karabiner.rb
@@ -1,87 +1,97 @@
-#!/usr/bin/env ruby
-
-require 'json'
-
-def gen_conf(rules)
-  {
-    global: {
-      check_for_updates_on_startup: true,
-      show_in_menu_bar: false,
-      show_profile_name_in_menu_bar: false,
-    },
-    profiles: [{
-      complex_modifications: {
-        parameters: {
-          "basic.to_if_alone_timeout_milliseconds": 500,
-        },
-        rules: rules
-      }
-    }]
-  }
+module Rule
+  module_function
+  def gen(desc, manipulators)
+    manipulators.each { |m|
+      m[:type] = "basic"
+    }
+    {
+      description: desc,
+      manipulators: manipulators.map { |m| m.merge({
+        type: "basic"
+      }) }
+    }
+  end
 end
 
-def pp(obj)
-  puts JSON.pretty_generate(obj)
-end
+module Cond
+  module_function
+  def gen_device_if(identifiers)
+    {
+      type: "device_if",
+      identifiers: identifiers
+    }
+  end
 
-def rule_gen(desc, manipulators)
-  manipulators.each { |m|
-    m[:type] = "basic"
-  }
-  {
-    description: desc,
-    manipulators: manipulators.map { |m| m.merge({
-      type: "basic"
-    }) }
-  }
-end
-
-def cond_gen_device_if(identifiers)
-  {
-    type: "device_if",
-    identifiers: identifiers
-  }
-end
-
-def modfrom_gen_mandatory(mods)
-  {
-    mandatory: mods
-  }
-end
-
-def modfrom_gen_optional(mods)
-  {
-    optional: mods
-  }
-end
-
-$modfrom_any = modfrom_gen_mandatory(["any"])
-
-$cond_is_internal_keyboard = cond_gen_device_if([
+  # module variables
+  def is_internal_keyboard
+    gen_device_if([
       {vendor_id: 1452},
       {product_id: 610}
-])
+    ])
+  end
+end
 
+module ModFrom
+  module_function
+  def gen_mandatory(mods)
+    {
+      mandatory: mods
+    }
+  end
 
-$key_region_alphabet = ('a'..'z').to_a
-$key_region_symbol_below = [
-  "comma",
-  "period",
-  "slash",
-  "semicolon",
-  "quote",
-  "open_bracket",
-  "close_bracket",
-  "backslash",
-]
-$key_region_symbol_above = [
-  "hyphen",
-  "equal_sign",
-]
-$key_region_number = (0..9).to_a
-$key_region_below = $key_region_alphabet + $key_region_symbol_below
-$key_region_above = $key_region_number + $key_region_symbol_above
-$key_region_whole = $key_region_below + $key_region_above
+  def gen_optional(mods)
+    {
+      optional: mods
+    }
+  end
+
+  # module variables
+  def optional_any
+    gen_optional(["any"])
+  end
+  def mandatory_fn
+    gen_mandatory(["fn"])
+  end
+end
+
+module KeyRegion
+  module_function
+  def alphabet
+    ('a'..'z').to_a
+  end
+
+  # module variables
+  def symbol_below
+    [
+      "comma",
+      "period",
+      "slash",
+      "semicolon",
+      "quote",
+      "open_bracket",
+      "close_bracket",
+      "backslash",
+    ]
+  end
+  def symbol_above
+    [
+      "hyphen",
+      "equal_sign",
+    ]
+  end
+  def number
+    (0..9).to_a
+  end
+  def below
+    alphabet + symbol_below
+  end
+  def above
+    number + symbol_above
+  end
+  def whole
+    below + above
+  end
+end
 
 class Layer
   @@cond_is_in_layer0 = {
@@ -91,7 +101,7 @@ class Layer
   }
   def initialize(layer_num, layer_mods, layer_keys)
     @layer_num = layer_num
-    @layer_modes = layer_mods
+    @layer_mods = layer_mods
     @layer_keys = layer_keys
 
     @rules = []
@@ -111,7 +121,7 @@ class Layer
 
   def layer_rules
     [
-      rule_gen(
+      Rule.gen(
         "layer #{@layer_num}: layer_mod-key",
         @layer_keys.map { |key|
           {
@@ -120,7 +130,7 @@ class Layer
             ],
             from: {
               key_code: key,
-              modifiers: $modfrom_any
+              modifiers: ModFrom.optional_any
             },
             to: [
               key_code: key,
@@ -147,50 +157,3 @@ class Layer1 < Layer
     @layer_keys -= ('a'..'c').to_a
   end
 end
-
-rules = [
-  rule_gen(
-    "(internal) fn-caps_lock: caps_lock",
-    [
-      {
-        conditions: [
-          $cond_is_internal_keyboard
-        ],
-        from: {
-          key_code: "caps_lock",
-          modifiers: $mod_mand_fn
-        },
-        to: [
-          {key_code: "caps_lock"}
-        ],
-      }
-    ],
-  )
-]
-
-# Layer1
-layer1_mods = [
-  "command",
-  "shift",
-  "option",
-]
-layer1_keys = $key_region_whole
-
-# Layer2
-
-layers = [
-  Layer1.new(layer1_mods, ['d'])
-]
-
-# concat rules from all layers
-layers.each { |layer| rules += layer.gen_rules() }
-
-#test
-pp(rules)
-
-# conf = gen_conf(rules)
-
-# pp(conf)
-
-# f = File.open("./karabiner.json", "w")
-# JSON.dump(conf, f)
