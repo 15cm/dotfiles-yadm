b51f92de6fa024d1fc2ff6455ee3e0e9d420131c
Sinkerine <git@15cm.net>
2018-08-16 19:30:51 -0400

    suppress kstart5 output

--- local/bin/refresh-kde.sh
+++ local/bin/refresh-kde.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/bin/sh -c 'kquitapp5 plasmashell && kstart5 plasmashell && exit'
+kquitapp5 plasmashell && kstart5 plasmashell > /dev/null 2>&1 &
 systemctl --user restart xsession.target
