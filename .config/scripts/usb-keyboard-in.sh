3baa3c7138c3571c4db9276fecb164b4cba24692
Sinkerine <git@15cm.net>
2018-09-12 10:33:57 -0400

    (udev): tune suspend config of the second usb-c port for monitor

--- .config/scripts/usb-keyboard-in.sh
+++ .config/scripts/usb-keyboard-in.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo 'auto' > /sys/bus/usb/devices/2-3/power/control
 if lsusb | grep -q -E 'feed.*(1307|6060)'; then
   su sinkerine -c "DISPLAY=:0 XDG_RUNTIME_DIR=/run/user/1000 systemctl --user stop laptop-keymap.service"
 else
