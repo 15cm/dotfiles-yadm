379c8f02df89ab51f94e9c5550ccec4487718c88
Sinkerine <git@15cm.net>
2019-01-17 08:55:34 -0500

    fix(refresh-xsession): fix multi-monitor checking

--- local/bin/refresh-xsession.sh
+++ local/bin/refresh-xsession.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 systemctl --user restart xsession.target
-kcminit kcm_touchpad
 
-# Tell current mode by connection of external keyboard
-
-# Desktop mode
+# External keyboard
 if lsusb | grep -q -E 'feed.*(1307|6060)'; then
   systemctl --user stop laptop-keymap.service
-  ~/local/bin/move-workspaces.sh
-# Laptop mode
 else
   systemctl --user restart laptop-keymap.service
 fi
+
+# External Display(2 screens)
+monitor_num=$(xrandr -q | grep ' connected' | wc -l)
+if [ $monitor_num != 1 ]; then
+  # Assign workspaces to screens
+  ~/local/bin/move-workspaces.sh
+fi
+
