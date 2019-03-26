13d9a2f09a7f352abae1b3b604ac144f5353d725
Sinkerine <git@15cm.net>
2019-03-26 13:14:34 -0400

    fix(desktop entries): fix bluez-git systemd service

--- local/bin/fix-applications.sh
+++ local/bin/fix-applications.sh
@@ -3,3 +3,6 @@ app_folder="/usr/share/applications"
 
 # plex
 sudo rm -f $app_folder/plexmediaplayer.desktop
+
+# bluez-git
+sudo sed -i 's/\(ExecStart=\).*/\1\/usr\/lib\/bluetoothd/' /usr/lib/systemd/system/bluetooth.service
