103879ba68546609629a430fcc778e887c41a992
Sinkerine <github@15cm.net>
2019-04-22 23:07:37 -0400

    feat(desktop-entries): fix unit file path of bluz-git

--- local/bin/fix-desktop-entries.sh
+++ local/bin/fix-desktop-entries.sh
@@ -1,6 +1,11 @@
 #/bin/bash
 app_folder="/usr/share/applications"
 
+# bluz-git
+sudo sed -i 's|\(ExecStart=\).*|\1/usr/lib/bluetoothd|' /usr/lib/systemd/system/bluetooth.service
+
 # plex
 sudo rm -f $app_folder/plexmediaplayer.desktop
 sudo sed -i 's/Exec=\S*$/& --desktop --scale-factor=2/' $app_folder/plex-media-player.desktop
+
+sudo systemctl daemon-reload
